fb13aeb208f9bf61de71979a878ae7a91e961829
ngothikieungan <167753665+ngothikieungan@users.noreply.github.com>
2024-06-11 16:24:29 +0700

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,8 @@
+
+function toggleNav() {
+    document.getElementById("myShoppingCart").style.width = "450px";
+}
+
+function closeNav() {
+    document.getElementById("myShoppingCart").style.width = "0";
+}
